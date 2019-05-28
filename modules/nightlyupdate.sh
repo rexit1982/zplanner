5130c934cb5284a2cac00a59e0a1229197893184
rexit1982 <rexit1982@gmail.com>
2019-05-28 14:14:33 -0400

    Update nightlyupdate.sh
    pull from rexit1982 fork

--- modules/nightlyupdate.sh
+++ modules/nightlyupdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Updating zPlanner from github"
-(cd /home/zerto/zplanner/ && git reset --hard HEAD && git pull http://www.github.com/zerto-ta-public/zplanner/)
+(cd /home/zerto/zplanner/ && git reset --hard HEAD && git pull http://www.github.com/rexit1982/zplanner/)
 echo "Running Update Helper Script"
 (/bin/bash /home/zerto/zplanner/updates.sh)
 
